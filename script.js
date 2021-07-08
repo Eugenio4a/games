8111432dba364ee253971cbdff802ca8719a9e92
Eugeniy e-marketing <u1-259@office107.local>
2021-07-08 15:16:18 +0300

    gamess

---
+++ script.js
@@ -0,0 +1,77 @@
+// import Swiper from 'https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js'
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    // },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
+
+
+var swiperNew = new Swiper(".new-swiper", {
+    effect: 'coverflow',
+    centeredSlides: true,
+    coverflowEffect: {
+        rotate: 230,
+        depth: 500,
+    },
+
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            coverflowEffect: {
+                rotate: 230,
+                depth: 500,
+
+
+            },
+        },
+        768: {
+            slidesPerView: 3,
+            coverflowEffect: {
+                rotate: 230,
+                depth: 300,
+
+
+            },
+        },
+        1024: {
+            slidesPerView: 3,
+            coverflowEffect: {
+                rotate: 230,
+                depth: 500,
+
+
+            },
+        },
+    }
+});
+
+// let hiddenMenuGame = document.querySelector('.hidden_menu');
+// let gamesBtn = document.querySelector('.games');
+// let consolesBtn = document.querySelector('.consoles');
+// let gamesMenu = document.querySelector('.game_menu');
+// let consolesMenu = document.querySelector('.consoles_menu');
+
+
+
+// function showMenu(btn, menu) {
+//     btn.addEventListener('mouseover', () => {
+//         menu.classList.toggle('hide');
+//     })
+
+// }
+// showMenu(gamesBtn, gamesMenu)
+// showMenu(consolesBtn, consolesMenu)
